294ef8e4dee6c46c03fd3e52fb5af8187dd14eec
Jason Kim <jasonwkim@google.com>
2011-07-26 14:31:54 -0700

    updated RunMergeTest to better track llvm changes

--- Sh/RunMergeTest.pl
+++ Sh/RunMergeTest.pl
@@ -50,21 +50,24 @@ my @x = grep {chomp} Piped("svnversion .", "");
 my $SVNVersion = $x[0];
 my $CurrRevTxt= "nacl-$SVNVersion-$LLVMRev-$LLVMGccRev";
 
-my ($CROSS_TARGET_ARM)   =qw(arm-none-linux-gnueabi);
-my ($CROSS_TARGET_X86_32)=qw(i686-none-linux-gnu);
-my ($CROSS_TARGET_X86_64)=qw(x86_64-none-linux-gnu);
-
 print "*********************************\n";
 print "This test is for REVISION $CurrRevTxt\n";
 my (@StartTime) = time;
-my (@Part1Time, @LLVMTime, @GccTime, @TestCompileTime, @TestRunTime, @SpecTime);
-my ($DoPart1, $DoLLVM, $DoGcc, $DoTest, $DoSpec, $TagSuccess) = (1, 1, 1, 1, 1, 0);
+my (@CompileTime, @TestRunTime, @SpecTime);
+my $ResetMQ     = 0;
+my $Clean       = 0;
+my $Download    = 1;
+my $Compile     = 1;
+my $DoTest      = 1; 
+my $DoSpec      = 1;
+my $TagSuccess = 0;
 
-$DoPart1 = 0 if (grep { /^-SkipPart1$/i } @ARGV);
-$DoLLVM = 0 if (grep { /^-SkipLLVM$/i } @ARGV);
-$DoGcc  = 0 if (grep { /^-SkipGCC$/i  } @ARGV);
+$ResetMQ = 1 if (grep { /^-ResetMQ$/i } @ARGV);
+$Clean = 1 if  (grep { /^-Clean$/i } @ARGV);
+$Compile = 0 if (grep { /^-SkipCompile$/i  } @ARGV);
 $DoTest = 0 if (grep { /^-SkipTest$/i } @ARGV);
 $DoSpec = 0 if (grep { /^-SkipSpec$/i } @ARGV);
+$Download = 0 if (grep { /^-SkipDownload$/i } @ARGV);
 my (@SkippedTests);
 
 push @SkippedTests, grep { /^x86/ } keys %TestArray if (grep { /^-SkipX86$/i } @ARGV);
@@ -72,6 +75,7 @@ push @SkippedTests, grep { /^x8664/ } keys %TestArray if (grep { /^-SkipX8664$/i
 push @SkippedTests, grep { /^x8632/ } keys %TestArray if (grep { /^-SkipX8632$/i } @ARGV);
 push @SkippedTests, grep { /^arm/ } keys %TestArray if (grep { /^-SkipARM$/i } @ARGV);
 push @SkippedTests, grep { /pic$/ } keys %TestArray if (grep { /^-SkipPIC$/i } @ARGV);
+
 if ($#SkippedTests >= 0) {
   @DeletedTests = delete @TestArray{@SkippedTests};
   print "Skipping the following tests: ", join(" ", @SkippedTests), "\n";
@@ -82,7 +86,7 @@ if ($#SkippedTests >= 0) {
 }
 
 $TagSuccess = 1 if (grep { /^-TagSuccess$/ } @ARGV);
-
+print "foo $Compile\n";
 sub ReportTime {
   my (@Times) = @_;
   my $Message = shift @Times;
@@ -92,105 +96,58 @@ sub ReportTime {
   print "*******************************************************************\n";
 }
 
-if ($DoPart1) {
-  push @Part1Time, "Part 1";
-  push @Part1Time, time;
-  &Shell("./tools/llvm/utman.sh check-for-trusted", "");
-  &Shell("./tools/llvm/utman.sh clean-install", "");
-  &Shell("./tools/llvm/utman.sh clean-logs", "");
-  &Shell("./tools/llvm/utman.sh binutils-arm", "");
-  push @Part1Time, time;
-  &ReportTime(@Part1Time);
+if ($Clean) {
+  &Shell("./tools/llvm/utman.sh clean", "wipeit");
+  &Shell("gclient sync");
 }
 
-if ($DoLLVM) {
-  push @LLVMTime, "LLVM Compile Time";
-  push @LLVMTime, time;
-  &Shell("./tools/llvm/utman.sh llvm-clean", "LLVM clean");
-  &Shell("./tools/llvm/utman.sh llvm", "LLVM");
-  &Shell("./tools/llvm/utman.sh driver", "");
-  push @LLVMTime, time;
-  &ReportTime(@LLVMTime);
-  &TagRepo($LLVMRepo, $LLVMLog{rev}, "CompileSuccess", "y") if ($TagSuccess);
+if ($Compile) {
+  push @CompileTime, "Compile Time";
+  push @CompileTime, time;
+
+  if ($ResetMQ) {
+    $ENV{UTMAN_RESET_MQ}="true";
+  } else {
+    $ENV{UTMAN_RESET_MQ}="false";
+  }
+  $ENV{PQ_REV_NAME_MOD}=Piped("git log | grep git-svn | head -1 | cut -d@ -f2 | sed -e 'print \$1'");
+  #$ENV{LLVM_QPARENT_REV}="svnrev(128002)";
+  #$ENV{LLVM_GCC_QPARENT_REV}="svnrev(126872)";
+  &Shell("./tools/llvm/utman.sh show-config", "");
+  &Shell("./tools/llvm/utman.sh everything-translator", "do it all");
+  push @CompileTime, time;
+  &ReportTime(@CompileTime);
 }
 
-##$Gold1 = "./toolchain/pnacl_linux_x86_64/arm-none-linux-gnueabi/lib/LLVMgold.so";
-
-if ($DoGcc) {
-  push @GccTime, "llvm-gcc compilation time"; 
-  push @GccTime, time;
-  &Shell("./tools/llvm/utman.sh  gcc-stage1-clean ${CROSS_TARGET_ARM}", "CLEAN llvm-gcc");
-  &Shell("./tools/llvm/utman.sh  gcc-stage1-clean ${CROSS_TARGET_X86_32}", "CLEAN llvm-gcc");
-  &Shell("./tools/llvm/utman.sh  gcc-stage1-clean ${CROSS_TARGET_X86_64}", "CLEAN llvm-gcc");
-  &Shell("./tools/llvm/utman.sh  gcc-stage1-sysroot", "");
-  &Shell("./tools/llvm/utman.sh  gcc-stage1 ${CROSS_TARGET_ARM}", "");
-  &Shell("./tools/llvm/utman.sh  gcc-stage1 ${CROSS_TARGET_X86_32}", "");
-  &Shell("./tools/llvm/utman.sh  gcc-stage1 ${CROSS_TARGET_X86_64}", "");
-  &Shell("./tools/llvm/utman.sh  rebuild-pnacl-libs", "");
-  &Shell("./tools/llvm/utman.sh misc-tools", "");
-  &Shell("./tools/llvm/utman.sh  verify", "");
-  push @GccTime, time;
-  &ReportTime(@GccTime);
-  &TagRepo($LLVMGccRepo, $LLVMGccLog{rev}, "CompileSuccess", "y") if ($TagSuccess);
+if ($Download) {
+  &Shell("./tools/llvm/utman.sh download-trusted", "");
 }
+
+
 
 if ($DoTest) {
-  if (0) {
-
-    push @TestCompileTime, "Test Compilation Time";
-    push @TestCompileTime, time;
-    my ($J8) = '-j8';
-    &Shell("./scons platform=x86-64 MODE=nacl,opt-host bitcode=1 ${J8}", "");
-    &Shell("./scons platform=x86-32 MODE=nacl,opt-host bitcode=1 ${J8}", "");
-    &Shell("./scons platform=arm    MODE=nacl,opt-host bitcode=1 ${J8}", "");
-
-    &Shell("./scons platform=x86-64 MODE=nacl,opt-host bitcode=1 nacl_pic=1  ${J8}", "");
-    &Shell("./scons platform=x86-32 MODE=nacl,opt-host bitcode=1 nacl_pic=1  ${J8}", "");
-    &Shell("./scons platform=arm    MODE=nacl,opt-host bitcode=1 nacl_pic=1  ${J8}", "");
-    push @TestCompileTime, time;
-    &ReportTime(@TestCompileTime);
-
-    &TagRepo($LLVMRepo, $LLVMLog{rev}, "TestCompileSuccess", "y") if ($TagSuccess);
-    &TagRepo($LLVMGccRepo, $LLVMGccLog{rev}, "TestCompileSuccess", "y") if ($TagSuccess);
-    print "**********************************************************************\n" .
-      "DONE WITH BUILD\n***********************************************************\n";
-
-    push @TestRunTime, "Test Run Time";
-    push @TestRunTime, time;
-
-    &Shell("./scons platform=x86-64 MODE=nacl,opt-host bitcode=1 ${J8} smoke_tests", "");
-    &Shell("./scons platform=x86-32 MODE=nacl,opt-host bitcode=1 ${J8} smoke_tests", "");
-    &Shell("./scons platform=arm    MODE=nacl,opt-host bitcode=1 ${J8} smoke_tests", "");
-
-    &Shell("./scons platform=x86-64 MODE=nacl,opt-host bitcode=1 nacl_pic=1 ${J8} smoke_tests", "");
-    &Shell("./scons platform=x86-32 MODE=nacl,opt-host bitcode=1 nacl_pic=1 ${J8} smoke_tests", "");
-    &Shell("./scons platform=arm    MODE=nacl,opt-host bitcode=1 nacl_pic=1 ${J8} smoke_tests", "");
-    push @TestRunTime, time;
-    &ReportTime(@TestRunTime);
+  push @TestRunTime, "Test Run Time";
+  push @TestRunTime, time;
+  my $x = keys %TestArray;
+  if ($x == $RUN_ALL_TESTS) {
+    &Shell("./tools/llvm/utman-test.sh test-all", "");
   } else {
-    push @TestRunTime, "Test Run Time";
-    push @TestRunTime, time;
-    my $x = keys %TestArray;
-    if ($x == $RUN_ALL_TESTS) {
-      &Shell("./tools/llvm/utman-test.sh test-all", "");
-    } else {
-      &Shell("./tools/llvm/utman-test.sh test-x86-64", "") if (exists $TestArray{x8664});      
-      &Shell("./tools/llvm/utman-test.sh test-x86-64-pic", "") if (exists  $TestArray{x8664pic});
-      &Shell("./tools/llvm/utman-test.sh test-x86-32", "") if (exists  $TestArray{x8632});
-      &Shell("./tools/llvm/utman-test.sh test-x86-32-pic", "") if (exists  $TestArray{x8632pic});      
-      &Shell("./tools/llvm/utman-test.sh test-arm", "")   if (exists  $TestArray{arm});
-      &Shell("./tools/llvm/utman-test.sh test-arm-pic", "") if (exists  $TestArray{armpic});
-    }
-    push @TestRunTime, time;
-    &ReportTime(@TestRunTime);
+    &Shell("./tools/llvm/utman-test.sh test-x86-64", "") if (exists $TestArray{x8664});      
+    &Shell("./tools/llvm/utman-test.sh test-x86-64-pic", "") if (exists  $TestArray{x8664pic});
+    &Shell("./tools/llvm/utman-test.sh test-x86-32", "") if (exists  $TestArray{x8632});
+    &Shell("./tools/llvm/utman-test.sh test-x86-32-pic", "") if (exists  $TestArray{x8632pic});      
+    &Shell("./tools/llvm/utman-test.sh test-arm", "")   if (exists  $TestArray{arm});
+    &Shell("./tools/llvm/utman-test.sh test-arm-pic", "") if (exists  $TestArray{armpic});
   }
+  push @TestRunTime, time;
+  &ReportTime(@TestRunTime);
 }
 
 {
   push @SpecTime, "Spec2K run time";
   push @SpecTime, time;
   my ($SETUP);
-  my ($SPEC_TESTS) = qw(176.gcc);
+  my ($SPEC_TESTS) = qw(176.gcc 254.gap);
   my ($OFFICIAL) = `(cd ~/Work/cpu2000-redhat64-ia32/; pwd)`;
   chomp $OFFICIAL;
   my @SpecSetUps = qw (SetupPnaclX8664Opt SetupPnaclArmOpt);
@@ -201,8 +158,8 @@ if ($DoTest) {
   if ($DoSpec) { 
     for $SETUP qw(@SpecSetUps) { 
       &Shell("(cd tests/spec2k; ./run_all.sh CleanBenchmarks ${SPEC_TESTS})", "clean gcc spec2k");
-      &Shell("(cd tests/spec2k; ./run_all.sh PopulateFromSpecHarness ${OFFICIAL} ${SPEC_TESTS})", "clean gcc spec2k");
-      &Shell("(cd tests/spec2k; ./run_all.sh BuildAndRunBenchmarks  ${SETUP} ${SPEC_TESTS})", "clean gcc spec2k");
+      &Shell("(cd tests/spec2k; ./run_all.sh PopulateFromSpecHarness ${OFFICIAL} ${SPEC_TESTS})", "clean specs");
+      &Shell("(cd tests/spec2k; ./run_all.sh BuildAndRunBenchmarks  ${SETUP} ${SPEC_TESTS})", "run specs");
     }
     push @SpecTime, time;
     &ReportTime(@SpecTime);
